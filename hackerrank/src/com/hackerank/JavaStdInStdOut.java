ffdb864ead256aa14b558b32806a961962cff9f5
DhanrajJ <DhanrajJ@DHANRAJJ-DT.ysi.yardi.com>
2018-11-02 17:51:37 +0530

    closed Scan

--- hackerrank/src/com/hackerank/JavaStdInStdOut.java
+++ hackerrank/src/com/hackerank/JavaStdInStdOut.java
@@ -8,10 +8,12 @@ public class JavaStdInStdOut {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
+        scan.close();
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c); 
+        
    }
 }
 
