b78262ea0aeafb23657440c8006a142ba4ec5f3a
jadhavdhanrajk <44357035+jadhavdhanrajk@users.noreply.github.com>
2018-11-02 14:38:26 +0530

    Update WelcomeToJava.java

--- hackerrank/src/com/hackerank/WelcomeToJava.java
+++ hackerrank/src/com/hackerank/WelcomeToJava.java
@@ -1,7 +1,6 @@
 package com.hackerank;
 
 public class WelcomeToJava {
-
     public static void main(String[] args) {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");   
